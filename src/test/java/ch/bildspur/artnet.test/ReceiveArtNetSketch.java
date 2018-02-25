ba3cf9cd762dd77d42681cd0a7bac49704beacf7
Florian Bruggisser <florian@nexpose.ch>
2018-02-25 17:30:14 +0100

    added artnet receive test

---
+++ src/test/java/ch/bildspur/artnet.test/ReceiveArtNetSketch.java
@@ -0,0 +1,59 @@
+package ch.bildspur.artnet.test;
+
+import ch.bildspur.artnet.ArtNetClient;
+import ch.bildspur.artnet.ArtNetNode;
+import processing.core.PApplet;
+
+
+public class ReceiveArtNetSketch extends PApplet {
+    public static void main(String... args) {
+        ReceiveArtNetSketch sketch = new ReceiveArtNetSketch();
+        sketch.run();
+    }
+
+    public void run()
+    {
+        runSketch();
+    }
+
+    ArtNetClient artnet = new ArtNetClient();
+    ArtNetNode localhostNode = new ArtNetNode("127.0.0.1");
+    byte[] dmxData = new byte[512];
+
+    @Override
+    public void settings()
+    {
+        size(500, 500, FX2D);
+    }
+
+    @Override
+    public void setup()
+    {
+        colorMode(HSB, 360, 100, 100);
+
+        artnet.start();
+    }
+
+    @Override
+    public void draw() {
+        int c = color(frameCount % 360, 50, 50);
+
+        background(c);
+        setRGB(0, c);
+
+        // send dmx
+        artnet.unicastDmx(localhostNode, 0, 0, dmxData);
+    }
+
+    @Override
+    public void stop() {
+        artnet.stop();
+    }
+
+    void setRGB(int address, int color)
+    {
+        dmxData[address] = (byte) red(color);
+        dmxData[address + 1] = (byte) green(color);
+        dmxData[address + 2] = (byte) blue(color);
+    }
+}
